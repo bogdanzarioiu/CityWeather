ee48478eecb8de070977a0f2a5c70cb762bb736d
Bogdan Zarioiu <bogdanzarioiu@gmail.com>
2021-03-15 22:07:57 +0100

    Initial Commit

---
+++ CityWeather/CityWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityWeatherApp.swift
+//  CityWeather
+//
+//  Created by Bogdan on 3/15/21.
+//
+
+import SwiftUI
+
+@main
+struct CityWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
